d1d98684a25eb795c1e8638f76885e7d22da6d49
Evan ngugi wambui <evanngugi547@gmail.com>
2026-03-15 02:16:18 +0300

    add link service

---
+++ backend/src/api/services/link.service.ts
@@ -0,0 +1,26 @@
+import base62 from "@sindresorhus/base62";
+import { FRONTEND_BASE_URI } from "../../config/env";
+import { User, Link } from "../../models";
+import { PaymentLink } from "../middlewares/validators";
+
+export const generatePaymentLink = async (linkData: PaymentLink) => {
+  const merchant = await User.findByPk(linkData.merchant_id);
+
+  if (!merchant) return { merchant, link: null };
+  const link = await Link.create({
+    invoice_no: linkData.invoiceNo || "",
+    shortCode: linkData.shortCode,
+    amount: linkData.amount,
+    merchant_id: merchant.id,
+    url: "",
+    expiresAt: linkData.expiresAt,
+  });
+
+  const base62String = base62.encodeString(link.id);
+  const baseUrl = FRONTEND_BASE_URI;
+  const url = `${baseUrl}/payments/payment-link?token=${base62String}`;
+  link.set("url", `${url}`);
+
+  await link.save();
+  return { merchant, link };
+};
