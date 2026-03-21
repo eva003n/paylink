9c9b23a6fafa581725820612b018ac3f047e8d1a
Evan ngugi wambui <evanngugi547@gmail.com>
2026-03-21 03:45:12 +0300

    add job processors

--- backend/src/jobs/payment/processors.ts
+++ backend/src/jobs/payment/processors.ts
@@ -11,12 +11,16 @@ import { mpesaClient } from "../../config/mpesa/mpesaclient";
 import { getTimeStamp } from "../../utils";
 import { PaymentData, PaymentQuery } from "./payment.type";
 import logger from "../../logger/logger.winston";
-import {PaymentSTKQueryResponse, PaymentSTKResponse } from "../../api/middlewares/validators";
+import {
+  PaymentSTKQueryResponse,
+  PaymentSTKResponse,
+} from "../../api/middlewares/validators";
 import { Payment, PaymentStatus } from "../../models";
 import { enqueueSTKPoll } from "../../queues";
 import { MAX_POLL_ATTEMPTS } from "../../constants";
+import {  enqueuePaymentReceipt } from "../../queues/pdf.queue";
 
-console.log(getTimeStamp())
+console.log(getTimeStamp());
 
 export const handleMpesaSTKPush = async (paymentData: PaymentData) => {
   const shortCode =
@@ -30,8 +34,9 @@ export const handleMpesaSTKPush = async (paymentData: PaymentData) => {
   //create a transaction before hand the used the transactions id as the account reference
 
   // Shortcode+Passkey+Timestamp
-  const base64String = Buffer.from(`${shortCode}${passkey}${timeStamp}`).toString("base64");
-
+  const base64String = Buffer.from(
+    `${shortCode}${passkey}${timeStamp}`,
+  ).toString("base64");
 
   let payload = {
     BusinessShortCode: shortCode, // mearchants's shortcode
@@ -49,7 +54,7 @@ export const handleMpesaSTKPush = async (paymentData: PaymentData) => {
       NODE_ENV === "production"
         ? PROD_MPESA_EXPRESS_CALLBACK_URL
         : MPESA_EXPRESS_SANDBOX_CALLBACK_URL,
-    AccountReference: paymentData.transactionId,
+    AccountReference: paymentData.phoneNumber,
     TransactionDesc: "Make online payment",
   };
 
@@ -61,27 +66,28 @@ export const handleMpesaSTKPush = async (paymentData: PaymentData) => {
     );
     const code = response.ResponseCode;
     if (code == 0) {
-      logger.info(`STP push to PhoneNumber:${paymentData.phoneNumber} successfull`);
+      logger.info(
+        `STP push to PhoneNumber:${paymentData.phoneNumber} successfull`,
+      );
 
       // if request suecessfull add  checkoutrequestid and add to queue for STK query
       const transaction = await Payment.findByPk(paymentData.transactionId);
       transaction?.set("checkout_request_id", response.CheckoutRequestID);
       await transaction?.save();
 
-    await enqueueSTKPoll({
+      await enqueueSTKPoll({
         transactionId: paymentData.transactionId,
         shortCode: paymentData.shortCode,
         checkoutRequestId: transaction?.checkout_request_id as string,
-        attempts: 0
+        attempts: 0,
       });
 
       logger.info(
         `Payment checkout query enqueued: CheckoutRequestID: ${response.CheckoutRequestID}`,
       );
-    }else {
-      logger.error(`STP push to PhoneNumber:${paymentData.phoneNumber} failed`)
-    } 
-
+    } else {
+      logger.error(`STP push to PhoneNumber:${paymentData.phoneNumber} failed`);
+    }
   } catch (error) {
     logger.error(
       `Error:${error.message}, Mpesa Stk push to PhoneNumber: ${paymentData.phoneNumber}`,
@@ -90,12 +96,16 @@ export const handleMpesaSTKPush = async (paymentData: PaymentData) => {
 };
 
 export const handleMpesaSTKPoll = async (paymentQuery: PaymentQuery) => {
-  const shortCode =
-    (NODE_ENV === "production" ? paymentQuery.shortCode : parseInt(MPESA_SANDBOX_SHORTCODE as string)) as number;
-  const passkey =
-    (NODE_ENV === "production"
+  const shortCode = (
+    NODE_ENV === "production"
+      ? paymentQuery.shortCode
+      : parseInt(MPESA_SANDBOX_SHORTCODE as string)
+  ) as number;
+  const passkey = (
+    NODE_ENV === "production"
       ? PROD_MPESA_EXPRESS_PASSKEY
-      : MPESA_EXPRESS_SANDBOX_PASSKEY) as string;
+      : MPESA_EXPRESS_SANDBOX_PASSKEY
+  ) as string;
   const timeStamp = getTimeStamp();
 
   const base64String = Buffer.from(
@@ -106,44 +116,55 @@ export const handleMpesaSTKPoll = async (paymentQuery: PaymentQuery) => {
     Password: base64String,
     Timestamp: timeStamp,
     CheckoutRequestID: paymentQuery.checkoutRequestId,
-  })
+  });
 
   try {
-  const response = await mpesaClient.request<PaymentSTKQueryResponse, string>("POST", "/stkpushquery/v1/query", payload);
-  const transaction = await Payment.findByPk(paymentQuery.transactionId)
-const code = response.ResultCode
-  if(code == 0) {
-    transaction?.set("status", PaymentStatus.Successful)
-
-
-    // generate payment receipt pdf
-
-    // send receipt via email
-  }
-  
-  // if user hasn't acted re-queue based on the maximum pools
-  if (code > 0) {
-    logger.info(
-      `Payment query with CheckoutID: ${paymentQuery.checkoutRequestId} requeued for STK quering`,
+    const response = await mpesaClient.request<PaymentSTKQueryResponse, string>(
+      "POST",
+      "/stkpushquery/v1/query",
+      payload,
     );
+    const transaction = await Payment.findByPk(paymentQuery.transactionId);
+    const code = response.ResultCode;
+    if (transaction && code == 0) {
+      transaction.set("status", PaymentStatus.Successful);
 
-    // inly queue when the max poll attempts is not reached
-    if (paymentQuery.attempts >= MAX_POLL_ATTEMPTS) {
-      paymentQuery.attempts++;
-      // re-queue
-      await enqueueSTKPoll(paymentQuery);
+      // generate payment receipt pdf
+      await enqueuePaymentReceipt({
+        name: transaction.email.split("@")[0],
+        email: transaction.email,
+        phoneNumber: transaction.phone_number,
+        reference: transaction.mpesa_ref,
+        amount: transaction.amount,
+        paymentType: "",
+        account: transaction.phone_number,
+        paybill: shortCode,
+      });
     }
 
-    // after all attempts the transaction is marked as failed
-    transaction?.set("status", PaymentStatus.Failed);
-  }
-// update transaction status
-  await transaction?.save()
+    // if user hasn't acted re-queue based on the maximum pools
+    if (code > 0) {
+      logger.info(
+        `Payment query with CheckoutID: ${paymentQuery.checkoutRequestId} requeued for STK quering`,
+      );
+
+      // inly queue when the max poll attempts is not reached
+      if (paymentQuery.attempts >= MAX_POLL_ATTEMPTS) {
+        paymentQuery.attempts++;
+        // re-queue
+        await enqueueSTKPoll(paymentQuery);
+      }
+
+      // after all attempts the transaction is marked as failed
+      transaction?.set("status", PaymentStatus.Failed);
+    }
+    // update transaction status
+    await transaction?.save();
     logger.info(
       `Transaction ID: ${transaction?.id} Status: ${transaction?.status}`,
     );
   } catch (error) {
-    logger.error(`Error while making stk query request:${error.message}`)
+    logger.error(`Error while making stk query request:${error.message}`);
   }
 };
 export const handlePaymentConfirmation = async () => {};
--- backend/src/jobs/pdf/processors.ts
+++ backend/src/jobs/pdf/processors.ts
@@ -1,5 +1,17 @@
-import { MpesaPayment } from "../payment/payment.type";
+import { enqueueReceiptEmail } from "../../queues/email.queue";
+import { generatePDFReceipt } from "./receipt.pdf";
+import { ReceiptContent } from "./receipt.type";
+import logger from "../../logger/logger.winston";
 
-export const handleReceiptGeneration = async(payment: MpesaPayment) => {
+export const handleReceiptGeneration = async (content: ReceiptContent) => {
+   try {
+     // generate pdf receipt
+     generatePDFReceipt(content);
 
+     // add email data to email queue
+     await enqueueReceiptEmail({ receiver: content.email });
+   }catch(error) {
+    logger.error(`Error pdf processor: ${error.message}`)
+
+   }
 }
