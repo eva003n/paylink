b992d28529b9865ed9a6608be4073089d188e9c5
Evan ngugi wambui <evanngugi547@gmail.com>
2026-03-28 10:33:46 +0300

    remove clerk  auth context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter } from "react-router-dom";
 
-const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
         <App />
-      </ClerkProvider>
   </StrictMode>,
 );
