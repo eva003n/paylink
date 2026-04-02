08af17573ec1f8f84913a7bba2cc80f7ede514be
Evan ngugi wambui <evanngugi547@gmail.com>
2026-04-02 15:04:26 +0300

    update tsconfig files and add tsconfig paths package

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
+console.log(path.resolve(__dirname, "../shared"));
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+    // alias: {
+    //   "@": path.resolve(__dirname, "src"),
+    //   "@shared": path.resolve(__dirname, "../shared")
+    // },
   },
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://localhost:8000/api/v1",
+        target: "http://localhost:8000",
         changeOrigin: true,
+        rewrite: (path) => path.replace("/api", "/api/v1")
       },
     },
+    fs: {
+      allow: [".."] // allow access outside root(frontend dir)
+    }
   },
 });
