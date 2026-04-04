677efd93badf2c784f6522f38e589bb5d42e0662
Evan ngugi wambui <evanngugi547@gmail.com>
2026-04-04 23:52:45 +0300

    delete files

--- frontend/src/pages/Clients/PaymentCheckOut.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PaymentCheckOut = () => {
-  return (
-    <div>PaymentCheckOut</div>
-  )
-}
-
-export default PaymentCheckOut
--- frontend/src/pages/Merchant/PaymentStatus.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PaymentStatus = () => {
-  return (
-    <div>PaymentStatus</div>
-  )
-}
-
-export default PaymentStatus
