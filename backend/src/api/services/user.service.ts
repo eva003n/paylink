84785d4f898d8e83236283a28d598d7d589eb394
Evan ngugi wambui <evanngugi547@gmail.com>
2026-04-04 23:59:07 +0300

    update user service

--- backend/src/api/services/user.service.ts
+++ backend/src/api/services/user.service.ts
@@ -1,9 +1,10 @@
 import { Merchant } from "src/models";
 import { Id } from "src/schemas/validators";
+import { UserDTO } from "../dto";
 
 export const fetchUser = async(id: Id) => {
     const merchant = await Merchant.findByPk(id)
 
-    return merchant
+    return UserDTO.create(merchant)
 
 }
