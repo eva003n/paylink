578521f104e210d23760ae70d840808c975613c8
Evan ngugi wambui <evanngugi547@gmail.com>
2026-04-05 01:16:05 +0300

    make logo a link to home page

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -2,15 +2,16 @@ import type { ClassValue } from 'class-variance-authority/types';
 import { Shield } from 'lucide-react';
 import React from 'react'
 import { cn } from '@/utils';
+import { Link } from 'react-router-dom';
 
 const Logo = ({className, color = "text-white"}: {className?: ClassValue, color?: string}) => {
   return (
-    <div className={cn("flex items-center gap-3", className && className)}>
+    <Link to="/" title="Paylink" className={cn("flex items-center gap-3", className && className)}>
       <div className="flex h-10 w-10 items-center justify-center rounded-xl bg-brand-600">
         <Shield className="h-5 w-5 text-white" />
       </div>
       <span className={cn("font-display text-xl font-bold", color && color)}>PayLink</span>
-    </div>
+    </Link>
   );
 }
 
