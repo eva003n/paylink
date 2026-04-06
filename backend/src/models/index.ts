b8f130422580031f1a69dd3e6e397f55fa934f19
Evan ngugi wambui <evanngugi547@gmail.com>
2026-04-06 23:31:55 +0300

    add model associations

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,7 +1,22 @@
 import {Merchant, UserRoles} from "./merchants.model"
 import {Payment, PaymentStatus} from "./payments.model"
-import { Link, LinkStatus } from "./link.model"
+import { Link } from "./link.model"
 import {Client} from "./client.model"
+
+// merchant associations
+Merchant.hasMany(Payment, {
+    foreignKey: "merchant_id"
+})
+Payment.belongsTo(Merchant, {
+  foreignKey: "merchant_id",
+});
+// client associations
+Client.hasMany(Payment, {
+  foreignKey: "client_id",
+});
+Payment.belongsTo(Client, {
+  foreignKey: "client_id",
+});
 export {
     Merchant,
     UserRoles,
@@ -9,5 +24,4 @@ export {
     PaymentStatus,
     Link,
     Client,
-    LinkStatus
 }
