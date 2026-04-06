670a070714e2a3fcceacc9c4b423ca46e791527e
Evan ngugi wambui <evanngugi547@gmail.com>
2026-04-06 23:31:33 +0300

    add link dto

--- backend/src/api/dto/index.ts
+++ backend/src/api/dto/index.ts
@@ -1,4 +1,4 @@
-import { Merchant } from "src/models";
+import { Link, Merchant } from "src/models";
 
 export class UserDTO {
   id: string;
@@ -13,6 +13,23 @@ export class UserDTO {
 
   static create(user: Merchant | null) {
     return new UserDTO(user);
+  }
+}
+export class LinkDTO {
+  id: string | undefined;
+  shortCode: number | undefined;
+  url: string | undefined;
+  expiresAt: Date | undefined 
+
+  constructor(link: Link | null) {
+    this.id = link?.id || "";
+    this.shortCode = link?.shortCode || 0 ;
+    this.url = link?.url || "";
+    this.expiresAt = link?.expiresAt
+  }
+
+  static create(link: Link | null) {
+    return new LinkDTO(link);
   }
 }
 
