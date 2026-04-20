726caa537da0c33540005db451267bd8db024ae1
Evan ngugi wambui <evanngugi547@gmail.com>
2026-04-20 17:15:46 +0300

    update email env config variables

--- backend/src/api/mail/email.ts
+++ backend/src/api/mail/email.ts
@@ -1,10 +1,9 @@
 import nodemailer from "nodemailer";
 import {
-  ETHEREAL_PASSWORD,
-  ETHEREAL_USERNAME,
-  NODE_ENV,
-  PROD_SMTP_MAIL_SERVER,
-  PROD_SMTP_MAIL_SERVER_PORT,
+  APP_EMAIL,
+  SMTP_PASSWORD,
+  SMTP_USERNAME,
+
   SMTP_MAIL_SERVER,
   SMTP_MAIL_SERVER_PORT,
 } from "../config/env";
@@ -14,16 +13,12 @@ import { template } from "./template";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 const config: SMTPTransport.Options = {
-  host: NODE_ENV === "production" ? PROD_SMTP_MAIL_SERVER : SMTP_MAIL_SERVER,
-  port: parseInt(
-    (NODE_ENV === "production"
-      ? PROD_SMTP_MAIL_SERVER_PORT
-      : SMTP_MAIL_SERVER_PORT) as string,
-  ),
-  secure: SMTP_MAIL_SERVER_PORT === "465",
+  host: SMTP_MAIL_SERVER,
+  port: parseInt(SMTP_MAIL_SERVER_PORT as string),
+  secure: Number(SMTP_MAIL_SERVER_PORT ) === 465,
   auth: {
-    user: ETHEREAL_USERNAME,
-    pass: ETHEREAL_PASSWORD,
+    user: SMTP_USERNAME,
+    pass: SMTP_PASSWORD,
   },
 };
 // create transporter
@@ -31,7 +26,7 @@ const transporter = nodemailer.createTransport(config);
 
 export const sendMail = async (receiver: string) => {
   const info = await transporter.sendMail({
-    from: "paylink@ethereal.email",
+    from: APP_EMAIL,
     to: receiver,
     subject: "Payment Confirmation – Receipt Attached",
     html: template(receiver.split("@")[0]),
